6b31bd6aea2bc34bd100847b96c5d22ad22a435a
CJ at ali-bj <changjun.z@gmail.com>
2025-08-26 14:06:18 +0800

    rename source file to pls.sh and update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ mv ~/.config/pls/pls.conf ~/.config/pls/pls_old.conf 2>/dev/null || true
 # Download to temp and install
 tmp=$(mktemp)
 echo "Downloading pls..."
-curl -fSL -# https://raw.githubusercontent.com/cjccjj/pls/main/pls -o "$tmp" || { echo "Download failed"; exit 1; }
+curl -fSL -# https://raw.githubusercontent.com/cjccjj/pls/main/pls.sh -o "$tmp" || { echo "Download failed"; exit 1; }
 
 chmod 755 "$tmp"
 sudo cp "$tmp" /usr/local/bin/pls
